9edc20669210f87f8bb10615c27a95f4e33ad1c6
Xin <xin@XindeMac-mini.local>
2024-01-04 15:31:48 +0800

    debugging wandb log

--- ft-lingo/finetune_lora.sh
+++ ft-lingo/finetune_lora.sh
@@ -2,5 +2,6 @@ python finetune.py \
     --use_lora True \
     --data_path ./data/lyrics.json \
     --output_dir ./out/ \
-    --per_device_train_batch_size 1 \
-    --model_max_length 256 \
+    --per_device_train_batch_size 16 \
+    --model_max_length 512 \
+    --logging_steps 1 
