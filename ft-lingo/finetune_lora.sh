861cf50828175b170ea6b245a32f908d4ed2f1da
Xin <xin@XindeMac-mini.local>
2024-01-04 11:26:35 +0800

    add ft bash script for parameters update

--- ft-lingo/finetune_lora.sh
+++ ft-lingo/finetune_lora.sh
@@ -1,10 +1,7 @@
 python finetune.py \
     --use_lora True \
     --data_path ./data/lyrics.json \
-    --train_format input-output \
     --output_dir ./out/ \
     --per_device_train_batch_size 1 \
     --model_max_length 256 \
     --learning_rate 2e-5 \
-    --logging_steps 1 \
-    --bf16 True 
