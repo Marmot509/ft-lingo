569230a3d3bc360bf96f4520f8d9c60ef36c04a6
Xin <xin@XindeMac-mini.local>
2024-01-04 15:37:51 +0800

    fix OOM

--- ft-lingo/finetune_lora.sh
+++ ft-lingo/finetune_lora.sh
@@ -2,6 +2,6 @@ python finetune.py \
     --use_lora True \
     --data_path ./data/lyrics.json \
     --output_dir ./out/ \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --model_max_length 512 \
     --logging_steps 1 
