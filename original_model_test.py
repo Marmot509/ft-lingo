b51c6d967be9151716d9f7aba58970c66f2c887f
Xin <xin@XindeMac-mini.local>
2024-01-05 16:25:42 +0800

    debug original model test

--- original_model_test.py
+++ original_model_test.py
@@ -6,4 +6,5 @@ model = AutoModelForCausalLM.from_pretrained("deeplang-ai/LingoWhale-8B", device
 prompt = input("Prompt: ")
 inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
 pred = model.generate(input_ids=inputs["input_ids"], max_new_tokens=500)
+pred = pred[0, inputs["input_ids"].shape[-1]:]
 print("Response:", tokenizer.decode(pred, skip_special_tokens=True))
